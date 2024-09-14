3cb52d3e6d8eaa105fcd1f6daf861a75e546f16a
Koby Grah <kobygrah24@gmail.com>
2024-09-14 16:09:20 -0700

    Backup onto remote repo

---
+++ Shared/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  Shared
+//
+//  Created by Koby Grah on 7/18/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
